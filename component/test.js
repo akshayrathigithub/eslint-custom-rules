1ab816186af97dbc0cbe5cb59fbabbe09241ee8b
Akshay Rathi <akshay.rathi@razorpay.com>
2023-09-21 15:24:49 +0530

    changes (#3)
    * changes
    
    * yml updated
    
    * more changes
    
    * yml updated
    
    * yml updated
    
    * yml
    
    * yml updated
    
    * yml updated

--- component/test.js
+++ component/test.js
@@ -5,23 +5,18 @@ function CurrentBalance () {
   const balance = 40
   return (
     <div className="balances-container">
-      <div className="bal-cont-header">
-        <div className="balances-lhs-container">
-        <Amount
+              <Amount
         value={Math.abs(balance)}
         currency="INR"
         className={balance < 0 ? 'negative-balance' : ''}
       />
+      <div className="bal-cont-header">
+        <div className="balances-lhs-container">
           <div className="balance-type-container">
             <p>Current Balance</p>
           </div>
           <div className="balance-amount-container">
             {balance < 0 && <p className="negative-marker">-</p>}
-            <Amount
-              value={Math.abs(balance)}
-              currency="INR"
-              className={balance < 0 ? 'negative-balance' : ''}
-            />
           </div>
         </div>
       </div>
